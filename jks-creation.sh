e42d6e4b328b7bfa769a44a29a6ac2dad1d6fa27
hemant-dugar <32013254+hemant-dugar@users.noreply.github.com>
2024-05-27 11:30:51 +0530

    Add files via upload

--- jks-creation.sh
+++ jks-creation.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+# !/bin/sh
 # 
 # File:   jks-creation.sh
 # Author: coder4
@@ -9,6 +9,13 @@
 # $2 = .key file
 # $3 = password
 
+# CMD #1
 openssl pkcs12 -export -in $1 -inkey $2 -out ssl.pkcs12 -password pass:$3
 
+# CMD #2
 keytool -importkeystore -srckeystore ssl.pkcs12 -srcstoretype pkcs12 -srcalias 1 -srcstorepass $3 -destkeystore ssl.jks -deststoretype jks -deststorepass $3
+
+# CMD #3
+# Following is added as Ubuntu shows the above command will generate a properietory jks and so the following 
+# should be used with password used in the first command to generate a generalized jks.
+keytool -importkeystore -srckeystore ssl.jks -destkeystore ssl.jks -deststoretype pkcs12
